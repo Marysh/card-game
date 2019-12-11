1d7bd3299806ef606b021f0162f9d81854949b45
marySh <sheremeta.m@gmail.com>
2019-12-11 18:00:47 +0200

    adding img

--- src/index.js
+++ src/index.js
@@ -2,7 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-let cardsArr = ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd',];
+// let cardsArr = ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd',];
+let cardsArr = [
+  {
+    src: 'https://cdn.fishki.net/upload/post/2018/07/15/2651316/3-3.jpg',
+    identifier: 'a'
+  },
+  {
+    src: 'https://cdn.fishki.net/upload/post/2018/07/15/2651316/3-3.jpg',
+    identifier: 'a'
+  },
+  {
+    src: 'http://depo.ua/static/media/2018-01-18/files/27835738_CONVAR2297.jpg',
+    identifier: 'b'
+  },
+  {
+    src: 'http://depo.ua/static/media/2018-01-18/files/27835738_CONVAR2297.jpg',
+    identifier: 'b'
+  },
+  {
+    src: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/5C93/production/_108799632_sarah-skinner_grab-life-by-the-_00003735.jpg',
+    identifier: 'c'
+  },
+  {
+    src: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/5C93/production/_108799632_sarah-skinner_grab-life-by-the-_00003735.jpg',
+    identifier: 'c'
+  },
+  {
+    src: 'https://znaj.ua/images/2019/11/13/Onu022WeMi1McVQKrhiBSDCyng1Lx9LiQr5NE6pr.jpeg',
+    identifier: 'd'
+  },
+  {
+    src: 'https://znaj.ua/images/2019/11/13/Onu022WeMi1McVQKrhiBSDCyng1Lx9LiQr5NE6pr.jpeg',
+    identifier: 'd'
+  },
+];
 
 
 class CardsWrapper extends React.Component {
@@ -24,12 +58,12 @@ class CardsWrapper extends React.Component {
   handleCardClick = (index) => {
     const {openedCards, cards, equalCards, nextTurn, playerOne, playerTwo} = this.state;
 
-    if (openedCards.length < 2) {
+    if (openedCards.length < 2 && !openedCards.includes(index) && !equalCards.includes(index)) {
       let newOpenedCards = [...openedCards, index];
       this.setState({openedCards: newOpenedCards});
 
       if (newOpenedCards.length === 2) {
-        if (cards[newOpenedCards[0]] !== cards[newOpenedCards[1]]) {
+        if (cards[newOpenedCards[0]].identifier !== cards[newOpenedCards[1]].identifier) {
           setTimeout(() => {
             this.setState({openedCards: [], nextTurn: !nextTurn});
           }, 1000);
@@ -43,22 +77,23 @@ class CardsWrapper extends React.Component {
             changedPlayer = {playerTwo: playerTwo + 1};
           }
           this.setState({equalCards: newEqualCards, openedCards: [], ...changedPlayer});
-
-          if (newEqualCards.length === cards.length) {
-            const {playerOne, playerTwo} = this.state;
-            alert(`Player 1: ${nextTurn ? changedPlayer : playerOne}, Player 2: ${!nextTurn ? changedPlayer : playerTwo}`);
-          }
-
-          // console.log(this.state.nextTurn);
         }
       }
     }
     // if (cardsArr.length === equalCards.length) alert('Game is over');
   };
+  resetCards = () => {
+    this.setState({
+      openedCards: [],
+      equalCards: [],
+      playerOne: 0,
+      playerTwo: 0,
+      nextTurn: true
+    })
+  };
 
   render() {
     const {cards, openedCards, equalCards, playerOne, playerTwo} = this.state;
-    console.log(playerTwo, playerOne);
     return (
       <div>
         <div className="cards-wrapper d-flex space-around flex-wrap">
@@ -71,20 +106,26 @@ class CardsWrapper extends React.Component {
                 equalCards={equalCards}
                 onCardClick={this.handleCardClick}
               >
-                {card}
+                <div className="img-wrapper">
+                  <img src={card.src} alt={card.identifier}/>
+                </div>
               </Card>
             )
           }
         </div>
-        <div className="result d-flex align-center space-around">
-          <div>
-            {
-              `Player 1: ${this.state.playerOne}, 
-              Player 2: ${this.state.playerTwo}`
-            }
+
+        <div className="result">
+          <div className="d-flex space-around">
+            <div>Player First: {playerOne}</div>
+            <div>Player Second: {playerTwo}</div>
           </div>
-          <button className="btn">Reset</button>
+          {
+            // equalCards.length === cards.length ? <button className="btn">Reset</button> : null
+            equalCards.length === cards.length &&
+            <button className="btn" onClick={this.resetCards}>Reset</button>
+          }
         </div>
+
       </div>
 
     )
@@ -92,10 +133,10 @@ class CardsWrapper extends React.Component {
 }
 
 class Card extends React.Component {
-  get classes() {
+  get cardClasses() {
     const {openedCards, index, equalCards} = this.props;
     let classes = 'card';
-    if (openedCards.includes(index) || equalCards.includes(index)) classes += ' front-side';
+    if (openedCards.includes(index) || equalCards.includes(index)) classes += ' card-opened';
     return classes;
   }
 
@@ -107,7 +148,9 @@ class Card extends React.Component {
   render() {
     const {children} = this.props;
     return (
-      <div className={this.classes} onClick={this.toggleCard}>{children}</div>
+      <div className={this.cardClasses} onClick={this.toggleCard}>
+        <div className="card-content front-side">{children}</div>
+      </div>
     )
   }
 }
